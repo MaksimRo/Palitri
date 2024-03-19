3de312bbb7d0175232007b617278bb5564640cce
MaksimRo <73811166+MaksimRo@users.noreply.github.com>
2024-03-19 16:59:02 +0900

    Delete pal.js

--- pal.js
+++
@@ -1,8 +0,0 @@
-function Changebackoff(){
-    var flashback=document.getElementById("flashback");
-    flashback.style.backgroundColor="white"
-}
-function addNum(colr){
-    var flashback=document.getElementById("flashback");
-    flashback.style.backgroundColor=colr;
-}
