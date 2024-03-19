54d16723d7320bc247010de0820e2974dc9024ad
MaksimRo <73811166+MaksimRo@users.noreply.github.com>
2024-03-19 16:51:29 +0900

    Add files via upload

---
+++ pal.js
@@ -0,0 +1,8 @@
+function Changebackoff(){
+    var flashback=document.getElementById("flashback");
+    flashback.style.backgroundColor="white"
+}
+function addNum(colr){
+    var flashback=document.getElementById("flashback");
+    flashback.style.backgroundColor=colr;
+}
